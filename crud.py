94e1ddc9f787a8d56684dc1ba555c329bfb3fce9
Marcus <marcus.fonseca968@gmail.com>
2026-03-17 17:09:29 -0300

    valida se lista está vazia ou não antes das opções

--- crud.py
+++ crud.py
@@ -41,62 +41,71 @@ while True:
 
     case 2:
       #listagem de cadastros
-      for i in range(len(nomeUsuario)) :
-        print(f'{i+1} - {nomeUsuario[i]}')
-
+      if (len(nomeUsuario) > 0):
+        for i in range(len(nomeUsuario)) :
+          print(f'{i+1} - {nomeUsuario[i]}')
+      else: 
+        print("Nenhum usuário cadastrado até o momento!")
 
     case 3:
       #editar usuario
-      for i in range(len(nomeUsuario)) :
-        print(f'{i+1} - {nomeUsuario[i]}')
-      
-      #validação de escolha do indice
-      while True:
-        try:
-          numEdit = int(input('Qual deseja editar?'))
-          if (numEdit >= 1 and numEdit <= len(nomeUsuario)):
-              break
-          else:
-              print("Digite uma opção válida, por favor. ")
-        except ValueError:
-            print("Letras não são aceitas, por favor insira números, de 1 a 5.")
+      if (len(nomeUsuario) > 0):
+        for i in range(len(nomeUsuario)) :
+          print(f'{i+1} - {nomeUsuario[i]}')
+        
+        #validação de escolha do indice
+        while True:
+          try:
+            numEdit = int(input('Qual deseja editar?'))
+            if (numEdit >= 1 and numEdit <= len(nomeUsuario)):
+                break
+            else:
+                print("Digite uma opção válida, por favor. ")
+          except ValueError:
+              print("Letras não são aceitas, por favor insira números, de 1 a 5.")
 
-      respostaEditar = input(f"Tem certeza que deseja editar o cadastro: '{nomeUsuario[numEdit - 1]}'? [s/n] ")
+        respostaEditar = input(f"Tem certeza que deseja editar o cadastro: '{nomeUsuario[numEdit - 1]}'? [s/n] ")
 
-      #validação de resposta
-      while ((respostaEditar != "s" and respostaEditar != "n")):
-        respostaEditar = input(f"Resposta inválida, digite 's' para sim ou 'n' para não ")
+        #validação de resposta
+        while ((respostaEditar != "s" and respostaEditar != "n")):
+          respostaEditar = input(f"Resposta inválida, digite 's' para sim ou 'n' para não ")
 
-      #edição
-      if (respostaEditar == "s"):
-        nomeUsuario[numEdit - 1] = (input('Insira o nome correto: '))
+        #edição
+        if (respostaEditar == "s"):
+          nomeUsuario[numEdit - 1] = (input('Insira o nome correto: '))
+      else:
+        print("Nenhum usuário cadastrado até o momento!")
+
 
     case 4:
       #deletar usuario
-      for i in range(len(nomeUsuario)) :
-        print(f'{i+1} - {nomeUsuario[i]}')
+      if (len(nomeUsuario) > 0):
+        for i in range(len(nomeUsuario)) :
+          print(f'{i+1} - {nomeUsuario[i]}')
 
-      while True:
-        try:
-          numDelete = int(input('Qual deseja deletar?'))
-          if (numDelete >= 1 and numDelete <= len(nomeUsuario)):
-              break
-          else:
-              print("Digite uma opção válida, por favor. ")
-        except ValueError:
-            print("Letras não são aceitas, por favor insira números, de 1 a 5.")
-            
-      respostaDeletar = input(f"Tem certeza que deseja deletar o cadastro: '{nomeUsuario[numDelete - 1]}'? [s/n] ")
+        while True:
+          try:
+            numDelete = int(input('Qual deseja deletar?'))
+            if (numDelete >= 1 and numDelete <= len(nomeUsuario)):
+                break
+            else:
+                print("Digite uma opção válida, por favor. ")
+          except ValueError:
+              print("Letras não são aceitas, por favor insira números, de 1 a 5.")
+              
+        respostaDeletar = input(f"Tem certeza que deseja deletar o cadastro: '{nomeUsuario[numDelete - 1]}'? [s/n] ")
 
-      #validação de resposta
-      while ((respostaDeletar != "s" and respostaDeletar != "n")):
-        respostaDeletar = input(f"Resposta inválida, digite 's' para sim ou 'n' para não ")
+        #validação de resposta
+        while ((respostaDeletar != "s" and respostaDeletar != "n")):
+          respostaDeletar = input(f"Resposta inválida, digite 's' para sim ou 'n' para não ")
 
-      #exclusão do cadastro
-      if (respostaDeletar == "s"):
-          del nomeUsuario[numDelete - 1]
-          print("Usuário deletado com sucesso!")
-
+        #exclusão do cadastro
+        if (respostaDeletar == "s"):
+            del nomeUsuario[numDelete - 1]
+            print("Usuário deletado com sucesso!")
+      else: 
+        print("Nenhum usuário cadastrado até o momento!")
+        
     case 5:
       #terminar o programa
       break
